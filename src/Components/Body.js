3e7040024772db3347f91cc027fd2f1a2fab9c79
Kevin Su <kevinsu@outlook.co.nz>
2021-09-28 14:11:19 +1300

    successfully displayed products from json and added loading screen

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 require('dotenv').config();
 
 function Body(props) {
+    // Structure to hold API response
+    const [data, setData] = useState([]);
 
-    const getProducts = async term => {
+    // Boolean to show loading animation while we fetch data from API 
+    const [loading, setLoading] = useState(true);
+
+    // Get products on first load
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const getProducts = () => {
         let req = new XMLHttpRequest();
 
         req.onreadystatechange = () => {
             if (req.readyState === XMLHttpRequest.DONE) {
+                const response = req.responseText;
+                setData(response);
                 console.log(req.responseText);
             }
         };
@@ -16,13 +28,19 @@ function Body(props) {
         req.open("GET", "https://api.jsonbin.io/b/5e9fc4a82940c704e1dc7893", true);
         req.setRequestHeader("X-Master-Key", process.env.REACT_APP_PRODUCTS_API_KEY);
         req.send();
-        console.log(process.env.REACT_APP_PRODUCTS_API_KEY);
+        
+        setLoading(false);
     }
 
     return (
         <div>
-            <h1>Body</h1>
-            <button onClick={getProducts}>Button</button>
+            {loading ? <h1>"Please wait while we load..."</h1> :
+                <div>
+                    <h1>Women's Clothing</h1>
+                    <h1>{data}</h1>
+                </div>
+            }
+            
         </div>
     );
 }
